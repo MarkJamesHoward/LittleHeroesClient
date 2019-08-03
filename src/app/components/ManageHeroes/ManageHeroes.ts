bc67418d47c5fb0e2dc509aebda72f0a48d8aeeb
MarkHoward <markjameshoward@hotmail.co.uk>
2019-08-03 20:10:54 +1200

    manage heres

--- src/app/components/ManageHeroes/ManageHeroes.ts
+++ src/app/components/ManageHeroes/ManageHeroes.ts
@@ -1,14 +1,10 @@
 import { Router } from "aurelia-router";
 import { inject } from "aurelia-framework";
 import { HttpClient } from "aurelia-fetch-client";
-import {
-  Parent,
-  SignedIn,
-  BrowniePoints,
-  IAvailableRewards
-} from "../../library/interfaces";
+import { Parent, SignedIn, BrowniePoints, IAvailableRewards } from "../../library/interfaces";
 import { DNS, dev } from "../global";
 import "@polymer/paper-button";
+import "@polymer/paper-input/paper-input.js";
 
 @inject(Router, HttpClient)
 export class ManageHeroes {
@@ -23,13 +19,24 @@ export class ManageHeroes {
   newUserInviteEmailAddress: string;
   displayError: boolean = false;
   errorMessage: string;
+  attemptingDelete: boolean = false;
 
   Home() {
     this.router.navigate("/welcome");
   }
 
   async RemoveChild(child: BrowniePoints) {
+    if (this.registeredKids.length > 1) {
+      for (var i = 0; i < this.registeredKids.length; i++) {
+        console.log(`checking ${i}`);
+        if (child.id === this.registeredKids[i].id) {
+          this.registeredKids.splice(i, 1);
+          console.log(`removed ${i}`);
+        }
+      }
+    }
     console.dir(child);
+    this.attemptingDelete = true;
     try {
       var result = await this.http.fetch(`${DNS}/api/Children/${child.id}`, {
         method: "delete",
@@ -37,26 +44,37 @@ export class ManageHeroes {
       });
       if (result.ok) {
         var data = await result.json();
-        this.registeredKids = data;
+        this.attemptingDelete = false;
+        //this.registeredKids = data;
         console.log("child removed successfully");
       } else {
-        this.errorMessage = "Failed to remove child " + await result.text();
+        this.attemptingDelete = false;
+        this.errorMessage = "Failed to remove child " + (await result.text());
         this.displayError = true;
         setTimeout(() => {
+          this.attemptingDelete = false;
           this.displayError = false;
-        }, 10000)
+        }, 10000);
       }
     } catch (e) {
+      this.attemptingDelete = false;
       this.errorMessage = "Failed to remove child " + e;
       this.displayError = true;
-     setTimeout(() => {
-      this.displayError = false;
-     }, 5000 )
+      setTimeout(() => {
+        this.attemptingDelete = false;
+        this.displayError = false;
+      }, 5000);
     }
   }
 
   ActivateAddChild() {
     this.addingChildActivated = true;
+    setTimeout(() => {
+      let box = document.querySelector("#InputBox");
+      console.log(box);
+      //@ts-ignore
+      box.focus();
+    }, 5);
   }
 
   Cancel() {
@@ -70,6 +88,13 @@ export class ManageHeroes {
   async AddChild() {
     this.loading = true;
 
+    let username = document.querySelector("#InputBox");
+
+    //@ts-ignore
+    this.newChildName = username.value;
+
+    // console.log(this.myusername)
+
     console.log("adding new child " + this.newChildName);
 
     try {
@@ -78,10 +103,10 @@ export class ManageHeroes {
         this.errorMessage = "Name must be greater than 3 characters";
         this.loading = false;
       } else {
-        var result = await this.http.fetch(
-          `${DNS}/api/Children/${this.newChildName}`,
-          { method: "post", credentials: "include" }
-        );
+        var result = await this.http.fetch(`${DNS}/api/Children/${this.newChildName}`, {
+          method: "post",
+          credentials: "include"
+        });
         if (result.ok) {
           var data = await result.json();
           this.registeredKids = data;
